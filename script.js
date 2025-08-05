51753c34619a9ade9dc21e5a2b7bcfa8454b8d3c
Tejas <tys.tejas.dev@gmail.com>
2025-08-05 16:20:52 +0530

    [New] - Reverting changes

---
+++ script.js
@@ -0,0 +1,31 @@
+  const whatsappBtn = document.getElementById('whatsapp-btn');
+  const callBtn = document.getElementById('call-btn');
+
+  let showWhatsApp = true;
+
+  // Initial state
+  whatsappBtn.style.display = 'flex';
+  callBtn.style.display = 'none';
+
+  setInterval(() => {
+    if (showWhatsApp) {
+      whatsappBtn.style.display = 'none';
+      callBtn.style.display = 'flex';
+    } else {
+      whatsappBtn.style.display = 'flex';
+      callBtn.style.display = 'none';
+    }
+    showWhatsApp = !showWhatsApp;
+  }, 5000); // 5 seconds toggle
+
+   const track = document.querySelector('.carousel-track');
+  const nextBtn = document.querySelector('.next-btn');
+  const prevBtn = document.querySelector('.prev-btn');
+
+  nextBtn.addEventListener('click', () => {
+    track.scrollBy({ left: 320, behavior: 'smooth' });
+  });
+
+  prevBtn.addEventListener('click', () => {
+    track.scrollBy({ left: -320, behavior: 'smooth' });
+  });
